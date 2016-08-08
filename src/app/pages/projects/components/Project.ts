974319d0256e550ef29b79c65903cd78b3ae51e3
moises <m.ramos@anca.org.ve>
2016-08-08 10:16:48 -0400

    DATEPICKER IN PROJECTS

--- src/app/pages/projects/components/Project.ts
+++ src/app/pages/projects/components/Project.ts
@@ -2,8 +2,8 @@ import { Hal } from '../../../shared/Hal'
 
 export class ProjectHal extends Hal {
   description : string;
-  //estimated_date_end: string;
-  //estimated_start_date: string;  
+  estimated_date_end: string;
+  estimated_start_date: string;  
   name:string;
 
 	constructor(){
