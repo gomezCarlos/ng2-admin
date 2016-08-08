e2cc1968f9c7fa3edc247f20265e0825e3091e3a
e-escalona-anca <euddy81@gmail.com>
2016-08-08 15:40:21 -0400

    Relacion fase-proyecto -listar-crear

--- src/app/pages/phase/components/create/components/form/form.component.ts
+++ src/app/pages/phase/components/create/components/form/form.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
-//import {ProjectService} from '../../../projectService';
-//import {ProjectHal} from '../../../Project';
+import {Component, OnInit} from '@angular/core';
+import {ProjectService} from '../../../../../projects/components/project.service';
+import {ProjectHal} from '../../../../../projects/components/Project';
 import { PhaseService } from '../../../phase.service';
 import { PhaseHal } from '../../../Phase';
 import { PaginatedList } from '../../../../../../shared/PaginatedList.component';
@@ -8,13 +8,14 @@ import { PaginatedList } from '../../../../../../shared/PaginatedList.component'
 @Component({
   selector: 'form',
   template: require('./form.html'),
-  providers: [PhaseService]
- //providers: [ProjectService],
+  providers: [PhaseService,ProjectService]  
 })
-export class Form {
+
+export class Form  implements OnInit{
+  listproject : PaginatedList<ProjectHal>;
   error : any;
   phasehal:PhaseHal;
-	constructor( private service : PhaseService) {
+	constructor( private service : PhaseService, private projects : ProjectService) {
 		this.phasehal = new PhaseHal();
     
   }
@@ -24,12 +25,23 @@ export class Form {
   //constructor(service : ProjectService ) {
  // }
 
- save(account : PhaseHal){
+ save(phase : PhaseHal){
+   phase.project=Number(phase.project);
  	this.service.save(phase).subscribe(response => {this.phasehal = response; alert("Fase Creada")},error => {this.error = error; alert(error.message)})
 
  }
 
- delete(account : PhaseHal){
+
+ ngOnInit(){
+    this.getprojects();
+  }
+
+ //OBTENER LISTA DE PROYECTOS
+ getprojects(){
+   this.projects.getPage(0).subscribe(response=>{this.listproject=response},error => {this.error = error; alert(error.message)})
+ }
+
+ delete(phase : PhaseHal){
  	this.service.delete(phase).subscribe(response => {alert("Fase Eliminada")},error => {this.error = error; alert(error.message)})
 
  }
