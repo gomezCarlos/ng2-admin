0f699c637145eefd5e5c5f48d0fb82421ca8ccdb
moises <m.ramos@anca.org.ve>
2016-08-18 20:00:20 -0400

    list jobs and create jobs

--- src/app/pages/jobs/components/job.service.ts
+++ src/app/pages/jobs/components/job.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {API_URL} from '../../../shared/api_url';
 import {Service} from '../../../shared/service.component';
 import { API_URL } from '../../../shared/api_url'
+import {UserService} from '../../../shared/user.service';
 import { JobHal } from './Job';
 import { Http } from '@angular/http';
 
 @Injectable()
 export class JobService extends Service<JobHal>{
        urlBackend=API_URL+"jobs"  
-	constructor( private _http : Http){
+	constructor( private _http : Http, private userService: UserService){
 	super(_http);}
 
 }
