20c1a45123d4401a0f94b80cbcd71dd0670d8fea
gomezCarlos <gomezcarlose@gmail.com>
2016-10-05 14:18:21 -0400

    ADD pieChart

--- src/app/pages/dashboard/components/pieChart/pieChart.component.ts
+++ src/app/pages/dashboard/components/pieChart/pieChart.component.ts
@@ -35,6 +35,7 @@ export class PieChart implements OnInit{
       let value=0;
       for(let chart of this.chartsRest){
         this.charts[i].stats= chart.stats;
+        this.charts[i].id= chart.id;
         value = Number(chart.stats);
         this.charts[i].description= chart.description;
         jQuery('.pie-charts .chart').each(function(index, chart) {
